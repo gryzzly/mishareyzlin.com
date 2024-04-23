126fec1933f6ed550eb2cff4bd61088ecd1cb811
Misha <mrejzlin@gmail.com>
2024-04-23 10:34:13 +0200

    fix insertion of scripts in content files (escape closing script tag when printing)

--- src/document.js
+++ src/document.js
@@ -16,7 +16,7 @@ export function document(content, data) {
         files, an unescaped closing script tag stored in content, for example,
         would break the whole page
       */''}
-      ${data && `<script>window.preloadedData = ${JSON.stringify(data)}</script>`}
+      ${data && `<script>window.preloadedData = ${JSON.stringify(data).replace(/<\/script>/g, '<\\/script>')}</script>`}
       <script type="module">
       import {start} from "/index.js";
       start();
